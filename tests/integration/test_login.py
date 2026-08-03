a2097a5a27a7a4675e749879be2c74befec64796
Marion.L <mrn.larue@gmail.com>
2026-08-03 11:57:19 +0200

    Ajout du test de connexion reussie

--- tests/integration/test_login.py
+++ tests/integration/test_login.py
@@ -1,5 +1,5 @@
+import server
 from server import app
-
 
 def test_email_inconnu_message_erreur():
     """Vérifie qu'un e-mail inconnu ne fait pas planter l'application."""
@@ -20,3 +20,44 @@ def test_email_inconnu_message_erreur():
     # Vérification du statut et du message affiché.
     assert reponse.status_code == 200
     assert "Désolé, cette adresse e-mail n’a pas été trouvée." in contenu_reponse
+
+
+def test_connexion_reussie(monkeypatch):
+    # Faux club adresse connue
+    clubs_tests = [
+        {
+            "name": "Club Test",
+            "email": "test@club.com",
+            "points": "13",
+        }
+    ]
+
+    # Fausse compèt
+    competitions_tests = [
+        {
+            "name": "Competition Test",
+            "date": "2026-08-03 10:00:00",
+            "numberOfPlaces": "25",
+        }
+    ]
+
+    # Remplacement des données
+    monkeypatch.setattr(server, "clubs", clubs_tests)
+    monkeypatch.setattr(server, "competitions", competitions_tests)
+
+    client_test = server.app.test_client()
+
+    # Connexion avec une adresse e-mail connue
+    reponse = client_test.post(
+        "/showSummary",
+        data={"email": "test@club.com"},
+    )
+
+    # Lecture du contenu
+    contenu_reponse = reponse.get_data(as_text=True)
+
+    # Vérification de la connexion
+
+    assert reponse.status_code == 200
+    assert "test@club.com" in contenu_reponse
+    assert "Points available: 13" in contenu_reponse
